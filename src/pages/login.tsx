1a05de8905a7dee6bc8549bf68ee92456f5888e5
Jason Wang <0212jason@gmail.com>
2023-10-08 16:09:13 +1100

    feat: implement interaction with backend server

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
+// Define the types for setUserId and setToken
+type SetUserIdFunction = (userId: string) => void;
+type SetTokenFunction = (token: string) => void;
+
+//PostData component
+const postData = async (data: any, setUserId: SetUserIdFunction, setToken: SetTokenFunction) => {
+  const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
+  if (!apiUrl) {
+    // Handle the case where the API URL is not defined
+    return console.error("BACKEND_API_URL is not defined.");
+  }
+  try {
+    const response = await fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.json();
+    setUserId(responseData.user);
+    setToken(responseData.token);
+    console.log(responseData.user, responseData.token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default function SignInPage() {
+  const [userId, setUserId] = useState("");
+  const [token, setToken] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("token", token);
+  }, [userId, token]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    postData(
+      {
+        email: email,
+        password: pwd,
+      },
+      setUserId,
+      setToken
+    );
+    console.log(userId, token);
+  };
   return (
     <div className="text-center m-5-auto">
       <h2>Sign in to us</h2>
-      <form action="/home" className="flex flex-col gap-[20px]">
+      <form method="post" className="flex flex-col gap-[20px]" onSubmit={handleSubmit}>
         <p>
           <label>Username or email address</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input
+            type="text"
+            name="email"
+            required
+            onChange={(event) => {
+              setEmail(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
         </p>
         <p>
           <label>Password</label>
@@ -17,7 +76,15 @@ export default function SignInPage() {
             <label className="right-label">Forget password?</label>
           </Link>
           <br />
-          <input type="password" name="password" required />
+          <input
+            type="password"
+            name="password"
+            required
+            onChange={(event) => {
+              setPwd(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
         </p>
         <p>
           <button id="sub_btn" type="submit">
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,26 +1,89 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 
+type RegistrationData = {
+  email: string;
+  password: string;
+};
+
+const postData = async (data: RegistrationData) => {
+  const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
+  if (!apiUrl) {
+    // Handle the case where the API URL is not defined
+    return console.error("BACKEND_API_URL is not defined.");
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/register`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.json();
+    console.log(responseData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default function SignUpPage() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // const data = new FormData(event.currentTarget);
+    postData({
+      email: email,
+      password: pwd,
+    });
+  };
   return (
     <div className="text-center m-5-auto">
       <h2>Join us</h2>
       <h5>Create your personal account</h5>
-      <form action="/home" className="flex flex-col gap-[20px]">
+      <form method="post" className="flex flex-col gap-[20px]" onSubmit={handleSubmit}>
         <p>
           <label>Username</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input
+            type="text"
+            name="first_name"
+            required
+            onChange={(event) => {
+              setUserName(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
         </p>
         <p>
           <label>Email address</label>
           <br />
-          <input type="email" name="email" required />
+          <input
+            type="email"
+            name="email"
+            required
+            onChange={(event) => {
+              setEmail(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
         </p>
         <p>
           <label>Password</label>
           <br />
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={(event) => {
+              setPwd(event.target.value);
+              console.log(event.target.value);
+            }}
+          />
         </p>
         <p>
           <input type="checkbox" name="checkbox" id="checkbox" required />{" "}
