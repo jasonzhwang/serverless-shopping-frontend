3fb512b688925cf59e54b899b16d5b8456f464f7
Jason Wang <0212jason@gmail.com>
2023-10-10 16:10:15 +1000

    fix: add Bearer auth on cart

--- src/pages/api/checkout_sessions/cart.ts
+++ src/pages/api/checkout_sessions/cart.ts
@@ -41,8 +41,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         metadata: { orderId: orderId },
       };
 
+      // Set the Authorization header with your Stripe API key
+      const requestOptions = {
+        headers: {
+          Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      };
+
       const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(
-        params
+        params,
+        requestOptions as any
       );
 
       res.status(200).json(checkoutSession);
